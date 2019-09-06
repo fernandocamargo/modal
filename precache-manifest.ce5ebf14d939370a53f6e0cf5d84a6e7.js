babed693737bbd0f9caeb7c346a735aad0bbad8e
Fernando Camargo <fernando.camargo@sellics.com>
2019-09-06 11:19:40 +0200

    Updates

---
+++ precache-manifest.ce5ebf14d939370a53f6e0cf5d84a6e7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cd6a382008915f63e45339b2e12d0b",
+    "url": "/modal/index.html"
+  },
+  {
+    "revision": "da5ee7f99922e8c6ee91",
+    "url": "/modal/static/js/2.e5288427.chunk.js"
+  },
+  {
+    "revision": "40d92e52f8226a14446b",
+    "url": "/modal/static/js/main.8947947a.chunk.js"
+  },
+  {
+    "revision": "c5ff501f644b93632a7f",
+    "url": "/modal/static/js/runtime~main.ef789876.js"
+  }
+]);
--- src/contexts/Modal/Modal.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const Modal = createContext();
-
-export default Modal;
